a4c261004bc78c3ffe88de2ebe038cffdeb9752e
Mohit <mohitverma7420@gmail.com>
2022-09-02 12:21:45 +0530

    1st

---
+++ app/src/main/java/com/example/firedw/SignUpActivcity.java
@@ -0,0 +1,14 @@
+package com.example.firedw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivcity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
